e43d202817139d6183ed381280f3edfc2a7d06ee
vlncceeks <vlncceeks@gmail.com>
2026-02-22 23:17:57 +0300

    fix tests

--- bot/src/main/java/backend/academy/linktracker/bot/BotStartUp.java
+++ bot/src/main/java/backend/academy/linktracker/bot/BotStartUp.java
@@ -3,9 +3,11 @@ package backend.academy.linktracker.bot;
 import backend.academy.linktracker.bot.runner.BotRunner;
 import com.pengrad.telegrambot.TelegramBot;
 import jakarta.annotation.PostConstruct;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
+@Profile("!test")
 public class BotStartUp {
     private final TelegramBot bot;
 
